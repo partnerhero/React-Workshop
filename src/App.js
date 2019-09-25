3ed6a0677b2f89f3c293b275147d58b141a0afec
Wragdan <davidfernandomendez@gmail.com>
2019-09-25 16:36:53 -0600

    uncommented

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ const App = () => {
     return (
         <div className='container'>
             <div className='centered'>
-                {/* <ColorSelector/> */}
+                <ColorSelector/>
                 <BasicForm />
             </div>
         </div>
